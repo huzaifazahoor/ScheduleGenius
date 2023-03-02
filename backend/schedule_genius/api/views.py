1deab19c7e66a31b000de08e3e20693e94cde20c
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2023-03-03 00:02:15 +0500

    Added ChatGPT code

--- backend/schedule_genius/api/views.py
+++ backend/schedule_genius/api/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from chatgpt_wrapper import ChatGPT
 
 @csrf_exempt
 def chat_gpt_api(request):
     if request.method == "POST":
-        input = request.POST.get("input") or None
-        return JsonResponse(
-            {
-                "input": input,
-                "output": ["* * * 24"],
-            }
-        )
+        try:
+            bot = ChatGPT()
+            user_input = request.POST.get("input") or None
+            response = bot.ask(
+                "Write the crontab syntax based on the following input. "
+                + "Just write syntax, don't write any other description.\n'"
+                + user_input
+                + "'",
+            )
+            tracemalloc.stop()
+            return JsonResponse(
+                {
+                    "input": user_input,
+                    "output": [response],
+                }
+            )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "error": str(e),
+                }
+            )
     elif request.method == "GET":
-        return JsonResponse({"message": "Please post request with input field"})
+        return JsonResponse({"error": "Please post request with input field"})
