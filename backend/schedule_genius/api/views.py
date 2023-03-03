2a12be45e40c2fe555a653c5a58ab70a4f930339
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2023-03-03 18:19:24 +0500

    Attached ChatGPT API

--- backend/schedule_genius/api/views.py
+++ backend/schedule_genius/api/views.py
@@ -1,31 +1,54 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from chatgpt_wrapper import ChatGPT
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 @csrf_exempt
 def chat_gpt_api(request):
     if request.method == "POST":
         try:
-            bot = ChatGPT()
             user_input = request.POST.get("input") or None
-            response = bot.ask(
-                "Write the crontab syntax based on the following input. "
-                + "Just write syntax, don't write any other description.\n'"
-                + user_input
-                + "'",
-            )
-            return JsonResponse(
-                {
-                    "input": user_input,
-                    "output": [response],
-                }
-            )
+            if user_input:
+                openai.api_key = config["API_KEY"]
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": f"Write the crontab syntax for the following text. Don't write the description.'{user_input}'",
+                        },
+                    ],
+                )
+                resp_output = (
+                    response.get("choices", [{}])[0].get("message", {}).get("content")
+                )
+                output = [
+                    clean_output(op.strip())
+                    for op in resp_output.split("\n")
+                    if op.strip() != ""
+                ]
+                return JsonResponse(
+                    {
+                        "input": user_input,
+                        "output": output,
+                        "AI_response": resp_output
+                    }
+                )
+            else:
+                e = "Please pass some input here."
         except Exception as e:
-            return JsonResponse(
-                {
-                    "error": str(e),
-                }
-            )
+            pass
+        return JsonResponse(
+            {
+                "error": str(e),
+            }
+        )
     elif request.method == "GET":
         return JsonResponse({"error": "Please post request with input field"})
+
+
+def clean_output(output):
+    return " ".join([word for word in output.split() if "command" not in word])
--- backend/schedule_genius/schedule_genius/settings.py
+++ backend/schedule_genius/schedule_genius/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)zb=(d%ngz1iuyq*t(^ybww$4j)!g3obhy$6t%+qn$wee_*)c0'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
